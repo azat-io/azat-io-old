360428d90fa55682dcf4619ccbea3e850d6607a3
Azat S <to@azat.io>
2022-08-19 16:11:28 +0300

    Apply 302 redirect to root page

--- pages/root.vue
+++ pages/root.vue
@@ -1,3 +1,25 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+let router = useRouter()
+
+if (!__VUEPRESS_SSR__) {
+  let isLang = (language: string | string[]): boolean => {
+    let checkLang = (lang: string): boolean =>
+      !!(window.navigator.language.indexOf(lang) + 1)
+    if (typeof language === 'string') {
+      return checkLang(language)
+    }
+    return language.some(lang => checkLang(lang))
+  }
+  if (isLang(['ru', 'uk', 'be'])) {
+    router.push('/ru')
+  } else {
+    router.push('/en')
+  }
+}
+</script>
+
 <template>
   <div />
 </template>
