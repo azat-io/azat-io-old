7725e9c0f2d6c851078f25409b4612617a3da164
Azat S <to@azat.io>
2022-09-05 15:37:40 +0300

    Add travel map

---
+++ components/country.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+import type { DefineComponent } from 'vue'
+
+interface Props {
+  name: string
+  originName: string
+  code: string
+}
+
+let props = defineProps<Props>()
+
+let icon: DefineComponent
+if (props.code === 'ae') {
+  icon = (await import(`~/assets/flag-ae.svg`)).default
+} else if (props.code === 'am') {
+  icon = (await import(`~/assets/flag-am.svg`)).default
+} else if (props.code === 'az') {
+  icon = (await import(`~/assets/flag-az.svg`)).default
+} else if (props.code === 'by') {
+  icon = (await import(`~/assets/flag-by.svg`)).default
+} else if (props.code === 'fr') {
+  icon = (await import(`~/assets/flag-fr.svg`)).default
+} else if (props.code === 'ge') {
+  icon = (await import(`~/assets/flag-ge.svg`)).default
+} else if (props.code === 'il') {
+  icon = (await import(`~/assets/flag-il.svg`)).default
+} else if (props.code === 'in') {
+  icon = (await import(`~/assets/flag-in.svg`)).default
+} else if (props.code === 'ke') {
+  icon = (await import(`~/assets/flag-ke.svg`)).default
+} else if (props.code === 'ps') {
+  icon = (await import(`~/assets/flag-ps.svg`)).default
+} else if (props.code === 'ru') {
+  icon = (await import(`~/assets/flag-ru.svg`)).default
+} else if (props.code === 'ua') {
+  icon = (await import(`~/assets/flag-ua.svg`)).default
+} else if (props.code === 'ug') {
+  icon = (await import(`~/assets/flag-ug.svg`)).default
+} else if (props.code === 'us') {
+  icon = (await import(`~/assets/flag-us.svg`)).default
+} else {
+  icon = (await import(`~/assets/flag-unknown.svg`)).default
+}
+</script>
+
+<template>
+  <div :class="$style.country">
+    <component :is="icon" :class="$style.icon" />
+    <div>
+      <span :class="$style.name" v-text="props.name" />
+      <span :class="$style['origin-name']" v-text="props.originName" />
+    </div>
+  </div>
+</template>
+
+<style module>
+.country {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: var(--size-s);
+  align-items: center;
+}
+
+.icon {
+  width: 48px;
+  margin-top: var(--size-xxs);
+}
+
+.name {
+  font-size: var(--font-size-s);
+  line-height: var(--line-height-s);
+  color: var(--color-brand);
+}
+
+.origin-name {
+  display: block;
+  font-size: var(--font-size-xxs);
+  line-height: var(--line-height-xxs);
+  color: var(--color-text);
+}
+</style>
