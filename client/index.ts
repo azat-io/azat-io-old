343dcdb153d42a3fc59b6d00a3ddf134cae80737
Azat S <to@azat.io>
2022-09-07 21:50:53 +0300

    Add timeline page

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,7 @@
 import { defineClientConfig } from '@vuepress/client'
 
 import TravelMap from '~/pages/travel-map.vue'
+import Timeline from '~/pages/timeline.vue'
 import Root from '~/pages/root.vue'
 import Home from '~/pages/home.vue'
 import '~/layout/colors.css'
@@ -11,6 +12,7 @@ import '~/layout/base.css'
 export default defineClientConfig({
   enhance({ app }) {
     app.component('travel-map', TravelMap)
+    app.component('timeline', Timeline)
     app.component('root', Root)
     app.component('home', Home)
   },
